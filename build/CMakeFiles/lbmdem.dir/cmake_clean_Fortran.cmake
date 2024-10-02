26ed01c145ecf90272c3e87916c49d1b7841983c
Qiujun Wang <57556070+gicyerlivan147@users.noreply.github.com>
2024-10-03 00:01:43 +0800

    Add files via upload

---
+++ build/CMakeFiles/lbmdem.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,38 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "cal_feq.mod"
+  "CAL_FEQ.mod"
+  "CMakeFiles/lbmdem.dir/cal_feq.mod.stamp"
+
+  "calfraction.mod"
+  "CALFRACTION.mod"
+  "CMakeFiles/lbmdem.dir/calfraction.mod.stamp"
+
+  "functions.mod"
+  "FUNCTIONS.mod"
+  "CMakeFiles/lbmdem.dir/functions.mod.stamp"
+
+  "gaussian_filter.mod"
+  "GAUSSIAN_FILTER.mod"
+  "CMakeFiles/lbmdem.dir/gaussian_filter.mod.stamp"
+
+  "global.mod"
+  "GLOBAL.mod"
+  "CMakeFiles/lbmdem.dir/global.mod.stamp"
+
+  "lbmparameters.mod"
+  "LBMPARAMETERS.mod"
+  "CMakeFiles/lbmdem.dir/lbmparameters.mod.stamp"
+
+  "loadfile.mod"
+  "LOADFILE.mod"
+  "CMakeFiles/lbmdem.dir/loadfile.mod.stamp"
+
+  "math.mod"
+  "MATH.mod"
+  "CMakeFiles/lbmdem.dir/math.mod.stamp"
+
+  "pbc.mod"
+  "PBC.mod"
+  "CMakeFiles/lbmdem.dir/pbc.mod.stamp"
+  )
---
+++ build/CMakeFiles/lbmdem.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for lbmdem.
+# This may be replaced when dependencies are built.
